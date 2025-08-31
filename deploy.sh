5a6597c68e90d1eec9b65573645e9e80954034ed
marsavar <mario.savarese@snyk.io>
2025-08-31 02:51:04 +0100

    remove blog

--- deploy.sh
+++ deploy.sh
@@ -1,28 +1,12 @@
 #!/usr/bin/env sh
 
-# abort on errors
 set -e
 
-# build
 npm run build
 
-# navigate into the build output directory
-cd dist
+REMOTE_HOST="marsavar"
+REMOTE_DIR="/var/www/home"
+LOCAL_PACKAGE_PATH="dist/"
 
-# add .nojekyll to bypass GitHub Page's default behavior
-touch .nojekyll
-
-# if you are deploying to a custom domain
-echo 'www.mariosavarese.com' > CNAME
-
-git init
-git add -A
-git commit -m 'deploy'
-
-# if you are deploying to https://<USERNAME>.github.io
-git push -f git@github.com:marsavar/marsavar.github.io.git main
-
-# if you are deploying to https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
-
-cd -
+echo "➡️ Copying $PACKAGE_NAME to remote host..."
+scp -r "$LOCAL_PACKAGE_PATH"* "$REMOTE_HOST:$REMOTE_DIR/"
